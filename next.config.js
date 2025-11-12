6848090799717dc2624f89875980982a01955563
Mousami Kalidindi <mousamikalidindi@mac.lan>
2025-11-11 18:03:02 -0600

    🚀 Configure for GitHub Pages deployment

--- next.config.js
+++ next.config.js
@@ -1,5 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  basePath: '/portfolio-mk',
+  images: {
+    unoptimized: true,
+  },
   reactStrictMode: true,
   swcMinify: true,
 }
